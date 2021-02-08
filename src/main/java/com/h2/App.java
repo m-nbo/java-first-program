b9d519ce286322d4e0246461f45a4ea2f36a28bd
m-nbo <62890523+m-nbo@users.noreply.github.com>
2021-02-08 22:39:45 +0100

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -2,16 +2,23 @@ package com.h2;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
+    }
+
+    public static int add(int[] numbers) {
+        int sum = 0;
+
+        for(int i = 0; i < numbers.length-1; i++) {
+            sum += numbers[i];
+        }
+
+        return sum;
     }
 }
---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,44 @@
+package com.h2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+
+    public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
+
+    public static void main(String[] args) {
+
+        String name;
+        int loanTermInYears;
+        float bestRate;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+
+        // Liest die Eingabe des Nutzers ein. Pausiert, solange kein Input erfolgt ist
+        name = scanner.nextLine();
+        System.out.print("Hello " + name);
+
+        // Liest die nächste Eingabe des Nutzers ein
+        System.out.println("Enter the loan term (in years)");
+        loanTermInYears = scanner.nextInt();
+
+        bestRate = getRates(loanTermInYears);
+
+        if(bestRate == 0.0f) {
+            System.out.println("No available rates for term: " + loanTermInYears +" years");
+        } else {
+            System.out.println("Available Rate: " + getRates(loanTermInYears) + "%");
+        }
+        scanner.close();
+
+    }
+
+    public static Float getRates(int loanTermInYears) {
+        if (bestRates.containsKey(loanTermInYears)) {
+            return bestRates.get(loanTermInYears);
+        } else
+        return 0.0f;
+    }
+}
