f4c6b33341463f03312818c3f2ea38d573d94607
m-nbo <62890523+m-nbo@users.noreply.github.com>
2021-02-10 23:40:06 +0100

    Completed module

--- src/main/java/com/h2/Finance.java
+++ src/main/java/com/h2/Finance.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 public class Finance {
+    
     public static final String BEST_LOAN_RATES = "bestLoanRates";
     public static final String SAVINGS_CALCULATOR = "savingsCalculator";
     public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
@@ -40,7 +41,7 @@ public class Finance {
                 MortgageCalculator.main(arguments);
                 return;
         }
-        return;
+
     }
 
     public static void main(String[] args) {
